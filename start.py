b799eddee7b6602eb2e99564cf725c209929d8b1
Zach <swellful@gmail.com>
2018-09-22 19:59:27 -0400

    update

--- start.py
+++ start.py
@@ -1,11 +1,19 @@
 import os
 from logo import *
+from variables import *
 
 print(chr(27) + "[2J")
 
 print(logo)
 
-if os.path.exists('./public'):
-	print("Public exists")
+if os.path.exists(publicDir):
+	print("Public Directory exists [✔]")
 else:
-	print("Public existsn't")
+	print("Creating Public Directory [" + publicDir + "]")
+	os.makedirs(publicDir)
+
+if os.path.exists(errorDir):
+	print("Error Directory exists [✔]")
+else:
+	print("Creating Error Directory [" + errorDir + "]")
+	os.makedirs(errorDir)
